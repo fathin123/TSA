74cdf4232ccc28cc94e9446df30ae0c4384fad99
fathin123 <fathinnaufalia@gmail.com>
2022-08-26 10:33:19 +0700

    Evaluasi 2

---
+++ Evaluasi2/menu.php
@@ -0,0 +1,18 @@
+<div class="container" style="margin-top: 80px">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="card">
+				<div class="card-header">
+					<h1 style="text-align: center">Data Transaksi</h1>
+					<form method="GET" action="test.php">
+  						<div id="menu" style="width:100%;padding-top:50px;background-image: Background.jpg; text-align:center;">
+  							<input type="text" name="nama_pelanggan" id="nama_pelanggan" placeholder="Nama Pelanggan" style="font-size:12px;"/>
+  							<input type="text" name="waktu_transaksi" id="waktu_transaksi" placeholder="Tanggal Transaksi" style="font-size:12px;"/>
+  							<input type="submit" id="cari" value="cari"/>
+  						</div>
+      				</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
